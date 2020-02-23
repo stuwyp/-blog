704d154583a1bd518846f0b181e2a76ff8f29897
stuwyp <stuypwang@163.com>
2020-02-23 12:40:55 +0800

    完善回收站功能

--- src/pages/RecycleBin/template.js
+++ src/pages/RecycleBin/template.js
@@ -1,5 +1,5 @@
 import blog from '@/api/blog.js'
-import tag from '@/api/tag.js'
+import {mapGetters} from 'vuex'
 
 export default {
   data() {
@@ -8,42 +8,34 @@ export default {
       total: 0,
       page: 1,
       pageCount: 0,
-      tagList: [],
-      filterTags: [],
       sortBy: 'time'
     }
   },
+  computed: {
+    ...mapGetters([
+      'user'
+    ])
+  },
   created() {
     this.page = parseInt(this.$route.query.page) || 1
-    blog.getBlogsByUserId({page: this.page, sortBy: this.sortBy}).then(res => {
+    blog.getRecycleBlogsByUserId({userId: this.user.id, page: this.page, sortBy: this.sortBy}).then(res => {
       console.log(res)
       this.blogs = res.data
       this.total = res.total
       this.page = res.page
       this.pageCount = res.pageCount
     })
-    tag.getTags().then(res => {
-      this.tagList = res.data
-    })
+
   },
   methods: {
     onPageChange(newPage) {
-      blog.getBlogsByUserId({page: newPage}).then(res => {
+      blog.getRecycleBlogsByUserId({userId: this.user.id, page: newPage}).then(res => {
         this.blogs = res.data
         this.total = res.total
         this.page = res.page
-        this.$router.push({path: '/archive/', query: {page: newPage}})
+        this.$router.push({path: '/recycle_bin/', query: {page: newPage}})
       })
     },
 
-    getBlogsByTags(tag_id){
-
-      blog.getBlogsByUserId({page: this.page, sortBy: this.sortBy, tag: tag_id}).then(res => {
-        this.blogs = res.data
-        this.total = res.total
-        this.page = res.page
-        this.pageCount = res.pageCount
-      })
-    }
   }
 }
--- src/pages/RecycleBin/template.vue
+++ src/pages/RecycleBin/template.vue
@@ -4,33 +4,16 @@
       <div class="block">
         <el-timeline>
           <el-timeline-item
-            v-if="blogs.length > 0"
             v-for="blog in blogs"
             :key="blog.id"
             :timestamp="friendlyDate(blog.updated_at)"
             placement="top">
             <el-card>
-              <router-link :to="`/blog/${blog.id}`">
-                <h3>{{blog.title}}</h3>
-                <div>{{blog.description}}</div>
-                <span v-if="blog.tags.length > 0" v-for="tag in blog.tags">{{tag.name}}</span>
-              </router-link>
-            </el-card>
-          </el-timeline-item>
-          <el-timeline-item
-            v-else
-            :timestamp="friendlyDate(new Date())"
-            placement="top">
-            <el-card>
-              暂无文章
-            </el-card>
-          </el-timeline-item>
-          <el-timeline-item
-            v-else
-            :timestamp="friendlyDate(new Date())"
-            placement="top">
-            <el-card>
-              <router-link to="/create">开始我的第一篇博客</router-link>
+              <h3>{{blog.title}}</h3>
+              <div>{{blog.description}}</div>
+              <span v-if="blog.tags.length > 0" v-for="tag in blog.tags">{{tag.name}}</span>
+              <!--<el-button type="mini">恢复</el-button>-->
+              <!--<el-button type="mini">删除</el-button>-->
             </el-card>
           </el-timeline-item>
         </el-timeline>
@@ -45,25 +28,6 @@
       </el-pagination>
 
     </section>
-    <section id="right-nav">
-      <div class="right-nav-top">热门标签</div>
-
-
-      <div class="right-tags">
-        <el-button
-          type="text"
-          class="tag-item"
-          v-for="item in tagList"
-          :key="item.name"
-          @click="getBlogsByTags(item.id)"
-          plain
-        >
-          {{ item.name }}
-        </el-button>
-      </div>
-
-    </section>
-
   </div>
 </template>
 
