11ae14ea931f74e9dce48274f506c0b5332b4e71
stuwyp <stuypwang@163.com>
2020-03-04 09:36:14 +0800

    【UPDATE】【代码优化】完善草稿编辑功能和自动保存功能（输入防抖动）

--- src/utils/index.js
+++ src/utils/index.js
@@ -78,41 +78,23 @@ export function formatTime(time, option) {
   }
 }
 
-export function debounce(func, wait, immediate) {
-  let timeout, args, context, timestamp, result
-
-  const later = function () {
-    // 据上一次触发时间间隔
-    const last = +new Date() - timestamp
-
-    // 上次被包装函数被调用时间间隔last小于设定时间间隔wait
-    if (last < wait && last > 0) {
-      timeout = setTimeout(later, wait - last)
-    } else {
-      timeout = null
-      // 如果设定为immediate===true，因为开始边界已经调用过了此处无需调用
-      if (!immediate) {
-        result = func.apply(context, args)
-        if (!timeout) context = args = null
-      }
-    }
-  }
-
-  return function (...args) {
-    context = this
-    timestamp = +new Date()
-    const callNow = immediate && !timeout
-    // 如果延时不存在，重新设定延时
-    if (!timeout) timeout = setTimeout(later, wait)
-    if (callNow) {
-      result = func.apply(context, args)
-      context = args = null
-    }
-
-    return result
-  }
+export function debounce(fn, wait) {
+  let timer = null;
+  console.log("???")
+  return function () {
+    if (timer !== null)
+      clearTimeout(timer);//如果多次触发将上次记录延迟清除掉
+    timer = setTimeout(() => {
+      fn.apply(this, arguments);
+    }, wait);
+  };
 }
+
 
 export function transformURL(url) {
   return process.env.BASE_API + url
+}
+
+export function genRandomStr() {
+  return Math.random().toString(16).substring(2) + Date.now().toString(16)
 }
--- src/views/draft/new.vue
+++ src/views/draft/new.vue
@@ -1,148 +1,351 @@
 <template>
-  <div class="container">
-    <div class="top">
-      <div class="title">
-        <el-input v-model="draft_title"
-                  class="input-title"
-                  placeholder="输入文章标题..."></el-input>
-      </div>
-      <div class="panel">
+  <div style="height: 100%">
+
+    <div v-if="match === 0" class="container">
+      <div class="top">
+        <div class="title">
+          <el-input v-model="draft_title"
+                    class="input-title"
+                    placeholder="输入文章标题..."
+                    @keyup.enter="changeEditStatus"></el-input>
+        </div>
+        <div class="panel">
         <span class="save">
-          <span v-if="saved">已保存至</span>
-          <span v-else>文章将会自动保存至</span>
-          <router-link class="draft-btn" to="/draft">草稿</router-link>
+          <span v-if="!isAutoSaved"></span>
+          <span v-else-if="!isEdited">文章将会自动保存至</span>
+          <span v-else-if="isSaved">已保存至</span>
+          <span v-else>正在保存中</span>
+
+          <router-link class="draft-btn" to="/editor/drafts">草稿</router-link>
         </span>
-        <i class="el-icon-picture" style="font-size: 22px;"></i>
+          <i class="el-icon-picture" style="font-size: 22px;"></i>
 
-        <el-popover v-model="visible" trigger="click">
-          <div class="pop-box">
-            <div class="publish-title">发布文章</div>
-            <div class="panel">
-              <div class="panel-title">分类</div>
+          <el-popover v-model="visible" trigger="click">
+            <div class="pop-box">
+              <div class="publish-title">发布文章</div>
+              <div class="panel">
+                <div class="panel-title">分类</div>
 
-              <div>
-                <el-button class="cate-btn"
-                           type="mini"
-                           v-for="cate in categories"
-                           :key="cate.id"
-                           @click="handleSelectCate(cate)">
-                  {{cate.name}}
-                </el-button>
+                <div>
+                  <el-select v-model="cate_id" placeholder="选择分类">
+                    <el-option
+                      v-for="item in categories"
+                      :key="item.id"
+                      :label="item.name"
+                      :value="item.id">
+                    </el-option>
+                  </el-select>
+                </div>
+
               </div>
-
+              <div class="panel">
+                <div class="panel-title">标签 (最多三个)</div>
+                <el-tag class="tag_margin"
+                        :key="tag.id"
+                        v-for="tag in tags"
+                        :closable="true"
+                        :close-trasition="true"
+                        @close="handleTagClose(tag)"
+                        type="primary"
+                >{{ tag }}
+                </el-tag>
+                <el-input
+                  class="input-new-tag"
+                  v-if="inputVisible"
+                  v-model="inputValue"
+                  ref="saveTagInput"
+                  size="mini"
+                  @keyup.enter.native="handleInputConfirm"
+                  @blur="handleInputConfirm"
+                >
+                </el-input>
+                <el-button v-else size="mini" @click="showInput">+ New Tag</el-button>
+              </div>
+              <div class="publish-btn">
+                <el-button type="small" @click="handlePublish">确定并发布</el-button>
+              </div>
             </div>
-            <div class="panel">
-              <div class="panel-title">标签 (最多三个)</div>
-              <el-tag class="tag_margin"
-                      :key="tag.id"
-                      v-for="tag in labels"
-                      :closable="true"
-                      :close-trasition="true"
-                      @close="handleTagClose(tag)"
-                      type="primary"
-              >{{ tag }}
-              </el-tag>
-              <el-input
-                class="input-new-tag"
-                v-if="inputVisible"
-                v-model="inputValue"
-                ref="saveTagInput"
-                size="mini"
-                @keyup.enter.native="handleInputConfirm"
-                @blur="handleInputConfirm"
-              >
-              </el-input>
-              <el-button v-else class="button-new-tag" size="mini" @click="showInput">+New Tag</el-button>
-            </div>
-            <div class="publish-btn">
-              <el-button type="small">确定并发布</el-button>
-            </div>
-          </div>
-          <el-button plain slot="reference" class="publish">
-            发布
-            <i v-if="!visible" class="el-icon-caret-bottom"/>
-            <i v-else class="el-icon-caret-top"/>
-          </el-button>
-        </el-popover>
 
-        <v-user class="user"/>
+            <el-button plain slot="reference" class="publish">
+              发布
+              <i v-if="!visible" class="el-icon-caret-bottom"/>
+              <i v-else class="el-icon-caret-top"/>
+            </el-button>
+          </el-popover>
 
+          <v-user class="user"/>
+
+        </div>
+      </div>
+      <div class="middle">
+        <mavon-editor
+          class="mavon-editor--box"
+          v-model="draft_content"
+          :autofocus="false"
+          placeholder="博客内容"
+          :ishljs="true">
+        </mavon-editor>
+      </div>
+      <div class="bottom">
+      <span class="bottom-panel">
+        <el-switch
+          v-model="isAutoSaved"
+          inactive-color="#ccc"
+          active-text="自动保存">
+        </el-switch>
+      </span>
       </div>
     </div>
-    <div class="middle">
-      <mavon-editor
-        class="mavon-editor--box"
-        v-model="draft_content"
-        :autofocus="false"
-        placeholder="博客内容"
-        :ishljs="true">
-      </mavon-editor>
+    <div v-else-if="match === -1">
+      <no-match/>
     </div>
-    <div class="bottom">
-      <span class="bottom-panel">
-        <i class="el-icon-picture"></i>
-      </span>
-    </div>
+    <div v-else></div>
+
   </div>
 </template>
 
 <script>
-import vUser from "../layout/components/user_tool";
+import noMatch from "../404.vue"
+import vUser from "../layout/components/user_tool.vue";
+import {genRandomStr, debounce} from "../../utils"
+import {mapGetters} from 'vuex'
+import cate from '@/api/cate'
+import blog from '@/api/blog'
+import draft from '@/api/draft'
+
+const OK = 0
+const NOT_EXIST = -1
+const NO_PERMISSION = -2
+// const DRAFT = {
+//   uuid: '',
+//   title: '',
+//   content: '',
+//   category_id: 0,
+//   tags: [],
+//   user_id: 0,
+//   description: ''
+// }
 
 export default {
   components: {
-    vUser
+    vUser,
+    noMatch
   },
+
   data() {
     return {
-      saved: false,
+      skip_content_watch: false,
+      skip_title_watch: false,
+      match: OK,
       draft_title: '',
       draft_content: '',
+      draft_desc: '',
       cate_id: 0,
-      labels: [],
+      uuid: '',
+
+      isSaved: false,
+      isAutoSaved: true,
       inputVisible: false,
       inputValue: '',
       visible: false,
-      categories: [
-        {id: 1, name: '后端'},
-        {id: 2, name: '前端'},
-        {id: 3, name: 'Android'},
-        {id: 4, name: 'iOS'},
-        {id: 5, name: '人工智能'},
-        {id: 7, name: '开发工具'},
-        {id: 8, name: '代码人生'},
-        {id: 6, name: '阅读'}
-      ]
+      isEdited: false,
+      isEditing: false,
+      categories: [],
+      tags: []
+
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'user'
+    ])
+  },
+  mounted() {
+    this.fetch_data()
+  },
+  watch: {
+    '$route'(to, from) {
+      console.log(to)
+      this.fetch_data(to, from)
+    },
+
+    draft_content() {
+      if (this.skip_content_watch) {
+        this.skip_content_watch = false
+        return
+      }
+      if (this.$route.path === '/editor/drafts/new') {
+        this.isEdited = true;
+        this.uuid = genRandomStr()
+        draft.createDraft({
+          title: this.draft_title,
+          content: this.draft_content,
+          user_id: this.user.id,
+          uuid: this.uuid
+        }).then(() => {
+          this.$message.success("创建成功")
+          this.$router.push({path: `/editor/drafts/${this.uuid}`})
+        })
+      }
+      if (this.isAutoSaved) {
+        this.isSaved = false
+        this.debounceUpdateDraft()
+      }
+    },
+    draft_title() {
+      if (this.skip_title_watch) {
+        this.skip_title_watch = false
+        return
+      }
+      if (this.$route.path === '/editor/drafts/new') {
+        this.isEdited = true;
+        this.uuid = genRandomStr()
+        draft.createDraft({
+          title: this.draft_title,
+          content: this.draft_content,
+          user_id: this.user.id,
+          uuid: this.uuid
+        }).then(() => {
+          this.$message.success("创建成功")
+          this.$router.push({path: `/editor/drafts/${this.uuid}`})
+        })
+      }
+      if (this.isAutoSaved) {
+        this.isSaved = false
+        this.debounceUpdateDraft()
+      }
     }
   },
   methods: {
-    handleSelectCate(cate) {
-      this.cate_id = cate.id
+    debounceUpdateDraft: debounce(async function () {
+      let data = {
+        title: this.draft_title,
+        content: this.draft_content,
+        description: this.draft_desc,
+        uuid: this.uuid,
+        category_id: this.cate_id,
+        tags: this.tags,
+      }
+      console.log("?")
+      await draft.updateDraft(data)
+      this.isSaved = true
+      this.isEdited = true;
+    }, 1500),
+    async getDraft() {
+      try {
+        console.log({uuid: this.$route.params.uuid, user_id: this.user.id})
+        let resp = await
+          draft.getDraftByUuid({uuid: this.$route.params.uuid, user_id: this.user.id})
+
+        this.draft_title = resp.data.title
+        this.draft_content = resp.data.content
+        this.uuid = this.$route.params.uuid
+        this.skip_title_watch = true
+        this.skip_content_watch = true
+        this.match = 0
+      }
+      catch (e) {
+        let status = e.response.status
+        if (status === 404) {
+          this.$message.error("不存在该文章")
+          this.match = NOT_EXIST
+        }
+        else if (status === 403) {
+          this.$message.error("当前用户无权更改此文章")
+          this.match = NO_PERMISSION
+        }
+      }
     },
-    handleTagClose: function (tag) {
-      this.labels.splice(this.labels.indexOf(tag), 1)
+    async fetch_data(to, from) {
+      try {
+        let res = await
+          cate.getCates()
+        this.categories = res.data
+        this.cate_id = 8
+        if (to.path === '/editor/drafts/new') {
+          this.match = 0
+          this.draft_title = ''
+          this.draft_content = ''
+          this.tags = []
+          this.skip_content_watch = true
+          this.skip_title_watch = true
+          this.isEdited = false
+          this.isEditing = false
+        } else if (from.path === '/editor/drafts/new') {
+          this.skip_content_watch = true
+          this.skip_title_watch = true
+        }
+        this.getDraft()
+      }
+      catch (e) {
+        console.log(e)
+      }
     },
-    handleInputConfirm: function () {
+
+    changeEditStatus() {
+      this.isEditing = true
+    },
+    checkValidation() {
+      if (this.tags.length === 0) {
+        this.$message.warning("请至少选择一个标签")
+        return false
+      }
+      else if (this.draft_title.trim() === '') {
+        this.$message.warning("博客标题不能为空")
+        return false
+      }
+      else if (this.draft_content.trim() === '') {
+        this.$message.warning("博客内容不能为空")
+        return false
+      }
+      else
+        return true
+    },
+    handlePublish() {
+      if (!this.checkValidation())
+        return
+
+      blog.createBlog({
+        title: this.draft_title,
+        content: this.draft_content,
+        description: '',
+        user_id: this.user.id,
+        tags: this.tags,
+        category_id: this.cate_id,
+        state: 1,
+        uuid: this.uuid
+      })
+        .then(() => {
+          // console.log(res)
+          this.$message.success("创建成功")
+          this.$router.push({path: `/published`})
+        })
+    },
+    handleTagClose(tag) {
+      this.tags.splice(this.tags.indexOf(tag), 1)
+    },
+    handleInputConfirm() {
       let inputValue = this.inputValue
       if (inputValue.trim()) {
 
-        if (this.labels.indexOf(inputValue) > -1)
+        if (this.tags.indexOf(inputValue) > -1)
           this.$message.error("标签重复，添加失败")
-        else if (this.labels.length >= 3)
+        else if (this.tags.length >= 3)
           this.$message.error("最多只能添加三个")
         else
-          this.labels.push(inputValue)
+          this.tags.push(inputValue)
 
       }
       this.inputVisible = false
       this.inputValue = ''
     },
-    showInput: function () {
+    showInput() {
       this.inputVisible = true
       this.$nextTick(() => {
         this.$refs.saveTagInput.focus()
       })
     },
+
   }
 }
 
@@ -198,7 +401,7 @@ export default {
           color: #c5c5c5;
         }
 
-        /deep/ .publish {
+        @{deep} .publish {
           font-size: 16px;
           border: 0;
           margin: 10px;
@@ -212,7 +415,7 @@ export default {
         }
 
         .el-icon-picture {
-          margin: 0 14px;
+          margin: 0 12px;
           cursor: pointer;
         }
 
@@ -249,8 +452,11 @@ export default {
         padding: 4px 20px;
         font-size: 20px;
 
-        .el-icon-picture {
-          color: #7e8c8d;
+        @{deep} .el-switch__label.el-switch__label--right {
+          color: #bbb;
+          &.is-active {
+            color: #409eef;
+          }
         }
       }
     }
@@ -263,7 +469,7 @@ export default {
     top: 60px !important;
     right: 40px;
     margin-top: 0;
-    width: 320px;
+    width: 200px;
     border-top: 1px solid #e2e2e2;
 
     .pop-box {
@@ -271,7 +477,7 @@ export default {
       .publish-title {
         margin-bottom: 10px;
         font-size: 17px;
-        font-weight: 600
+        font-weight: 600;
       }
       .panel-title {
         margin: 8px 0;
@@ -281,32 +487,29 @@ export default {
       .panel {
         margin: 10px 0;
       }
-      .cate-btn {
-        min-width: 40px;
-        max-width: 90px;
-        margin: 4px;
+      .input-new-tag {
+        margin: 6px 0;
+        border: 0;
+
+        .el-input__inner {
+          font-size: 14px;
+          height: 24px;
+          line-height: 24px;
+          border-radius: 0;
+          border: 0;
+          border-bottom: 1px solid #dddddd;
+          padding: 12px 4px;
+        }
       }
 
-      .input-new-tag {
-        margin: 4px 0;
-      }
       .el-button.el-button--mini {
         width: 80px;
-        padding: 8px 4px;
+        padding: 10px 8px;
       }
       .tag_margin {
         margin: 3px;
       }
 
-      /deep/ .el-input__inner {
-        font-size: 12px;
-        height: 20px;
-        line-height: 20px;
-        border-radius: 0;
-        border: 0;
-        border-bottom: 1px solid #dddddd;
-        padding: 10px 0;
-      }
       .publish-btn {
         margin-top: 20px;
         display: flex;
